1ddf54d3923c29be96ade9de0d53ea424588a880
Evita Orrock <evita.orrock@gmail.com>
2024-08-28 08:41:23 +0000

    Convert server to CommonJS module

---
+++ server.cjs
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
