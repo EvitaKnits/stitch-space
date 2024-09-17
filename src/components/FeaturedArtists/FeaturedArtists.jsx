19b50d1bffa8df3a3b90a9125ace9c1f67d5ae29
Evita Orrock <evita.orrock@gmail.com>
2024-09-17 11:33:14 +0000

    Add shaded background to caption on home page slides

--- src/components/FeaturedArtists/FeaturedArtists.jsx
+++ src/components/FeaturedArtists/FeaturedArtists.jsx
@@ -14,25 +14,20 @@ const FeaturedArtists = () => {
         <Carousel className={styles.Carousel} activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
                 <ExampleCarouselImage text="First slide" />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <Carousel.Caption className={styles.CarouselCaption}>
+                    <h3>James Robinson - Trees in Autumn - Embroidery</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <ExampleCarouselImage text="Second slide" />
-                <Carousel.Caption>
+                <Carousel.Caption className={styles.CarouselCaption}>
                     <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <ExampleCarouselImage text="Third slide" />
-                <Carousel.Caption>
+                <Carousel.Caption className={styles.CarouselCaption}>
                     <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
