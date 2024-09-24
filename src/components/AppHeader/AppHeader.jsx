bb5746477714a876e393e13c0cb9d59e947b298b
Evita Orrock <evita.orrock@gmail.com>
2024-09-24 13:03:54 +0000

    Remove tagline in title when user's name is there

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -17,22 +17,26 @@ const AppHeader = () => {
             <span className='d-none d-sm-inline'>&apos;s</span>
         </h1>) : ''
 
-    return (
-        <>
-            <header className={`App-header ${styles['App-header']}`}>
-                <Stack direction='horizontal' gap={2}>
-                    <FontAwesomeIcon icon={faStroopwafel} size='3x' className='icons' />
-                    {title}
-                    {/* Hides 'StitchSpace' on small screens if the user's name is in the title */}
-                    <h1 className={title && 'd-none d-sm-inline'}>Stitch Space</h1>
-                    {/* Hides the tagline on different breakpoints depending on whether the user's name is in the title */}
-                    <h2 className={`d-none ${title ? 'd-lg-inline' : 'd-md-inline'} text-truncate`}>- the fibre art community</h2>
-                </Stack>
-                <NavBar />
-            </header>
-            <Outlet />
-        </>
-    )
+return (
+    <>
+        <header className={`App-header ${styles['App-header']}`}>
+            <Stack direction='horizontal' gap={2}>
+                <FontAwesomeIcon icon={faStroopwafel} size='3x' className='icons' />
+                {title}
+                {/* Hides 'StitchSpace' on small screens if the user's name is in the title */}
+                <h1 className={title && 'd-none d-sm-inline'}>Stitch Space</h1>
+                {/* Only render the tagline when the user's name is not in the title */}
+                {!title && (
+                    <h2 className={`d-none d-md-inline text-truncate`}>
+                        - the fibre art community
+                    </h2>
+                )}
+            </Stack>
+            <NavBar />
+        </header>
+        <Outlet />
+    </>
+);
 }
 
 export default AppHeader
