958a9490c40d90741b4b1befd64b0d7b0f1c9ce7
Evita Orrock <evita.orrock@gmail.com>
2024-09-30 08:41:11 +0000

    Add Fade in to the inputs on the explore page

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -4,6 +4,7 @@ import SearchBar from '../../components/SearchBar/SearchBar';
 import ArtTypeDropdown from '../../components/ArtTypeDropdown/ArtTypeDropdown';
 import SortByDropdown from '../../components/SortByDropdown/SortByDropdown';
 import usePiecesList from '../../hooks/usePiecesList';
+import { Fade } from "react-bootstrap";
 
 const Explore = () => {
     const {
@@ -17,22 +18,24 @@ const Explore = () => {
     return (
         <div className="p-3">
             <h5>Search by piece or artist name, or leave blank and explore with filters</h5>
-            <Stack direction="horizontal" gap={4} className="flex-wrap p-3 justify-content-center">
-                <div className="flex-fill">
-                    <SearchBar params={params} setParams={setParams} />
-                </div>
-                <Stack direction="horizontal" gap={4} className="flex-no-wrap">
-                    <div>
-                        <ArtTypeDropdown params={params} setParams={ setParams} />
+            <Fade in={!loading}>
+                <Stack direction="horizontal" gap={4} className="flex-wrap p-3 justify-content-center">
+                    <div className="flex-fill">
+                        <SearchBar params={params} setParams={setParams} />
                     </div>
-                    <div className="vr" />
-                    <div>
-                        <SortByDropdown params={params} setParams={setParams} />
-                    </div>
+                    <Stack direction="horizontal" gap={4} className="flex-no-wrap">
+                        <div>
+                            <ArtTypeDropdown params={params} setParams={setParams} />
+                        </div>
+                        <div className="vr" />
+                        <div>
+                            <SortByDropdown params={params} setParams={setParams} />
+                        </div>
+                    </Stack>
                 </Stack>
-            </Stack>
+            </Fade>
             {!loading && pieces && (
-                <MasonryWall pieces={pieces}  {...rest}/>
+                <MasonryWall pieces={pieces}  {...rest} />
             )}
         </div>
     );
